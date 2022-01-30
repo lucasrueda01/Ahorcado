47ca2c1e38930bfd4a51c0843be59659303fb949
lucasrueda01 <71470879+lucasrueda01@users.noreply.github.com>
2022-01-30 13:47:40 -0300

    Añadido limite de intentos

--- Proyectos Python/Ahorcado/ahorcado.py
+++ Proyectos Python/Ahorcado/ahorcado.py
@@ -1,20 +1,32 @@
+#from curses.ascii import isalpha
 import random
 
-palabras = ['Programacion','Calamar','Teclado','Homosexual','Travesti','Control']
+palabras = ['programacion','calamar','teclado','homosexual','travesti','control','salamandra','juego','cafe']
 
 def jugar(palabra):
     incognita = len(palabra) * '_'
     Juego = True
+    intentos = 5
+
     while Juego:
         if incognita.find('_') == -1:
-            print(f'Felicidades! Encontraste la palabra: {incognita}')
+            print('')
+            print(f'Felicidades! Encontraste la palabra: {incognita.capitalize()}')
             break
-        print(f'--------Tu palabra: {incognita} ----------')
+
+        print('')
+        print(f'      Tu palabra: {incognita}')
         print('')
         miLetra = input('Ingrese una letra: ')
-        while not isalpha(miLetra) or len(miLetra) != 1:
-            print('Error: Debe ingresar una sola letra')
+
+        #while not isalpha(miLetra) or len(miLetra) != 1:
+            #print('Error: Debe ingresar una sola letra')
+            #miLetra = input('Ingrese una letra: ')
+
+        while incognita.find(miLetra) != -1:
+            print('La letra ingresada ya ha sido descubierta')
             miLetra = input('Ingrese una letra: ')
+
         if palabra.find(miLetra) != -1:
             ocurrencias = []
             i = 1
@@ -32,7 +44,13 @@ def jugar(palabra):
                 lista_incognita[j] = miLetra
                 incognita = ''.join(lista_incognita)
         else:
-            print('La letra no se encuentra en la palabra')
+            intentos -= 1
+            if intentos > 0:
+                print('La letra no se encuentra en la palabra')
+                print(f'Te quedan {intentos} intentos')
+            else:
+                print(f'Te quedaste sin intentos! La palabra era {palabra}')
+                break
 
 
 palabra = random.choice(palabras).lower()
