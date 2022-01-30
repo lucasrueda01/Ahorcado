7098577b37ecd540fc67953121756828700b9240
lucasrueda01 <71470879+lucasrueda01@users.noreply.github.com>
2022-01-30 14:40:36 -0300

    Menu añadido y varias cosas

--- Ahorcado/ahorcado.py
+++ Ahorcado/ahorcado.py
@@ -1,29 +1,34 @@
 #from curses.ascii import isalpha
 import random
+import os
 
 palabras = ['programacion','calamar','teclado','homosexual','travesti','control','salamandra','juego','cafe']
 
 def jugar(palabra):
     incognita = len(palabra) * '_'
     Juego = True
-    intentos = 5
+    intentos = 10
+    letras_erradas = []
 
     while Juego:
         if incognita.find('_') == -1:
             print('')
             print(f'Felicidades! Encontraste la palabra: {incognita.capitalize()}')
             break
-
+        
+        os.system('cls')
+        print(f'Tu palabra: {incognita}')
         print('')
-        print(f'      Tu palabra: {incognita}')
-        print('')
+        print(f'Te quedan {intentos} intentos')
+        if letras_erradas:
+            print(f'Letras erradas: {letras_erradas}')
         miLetra = input('Ingrese una letra: ')
 
-        #while not isalpha(miLetra) or len(miLetra) != 1:
+        #while not isalpha(miLetra) or len(miLetra) != 1 or miLetra == '':
             #print('Error: Debe ingresar una sola letra')
             #miLetra = input('Ingrese una letra: ')
 
-        while incognita.find(miLetra) != -1:
+        while (incognita.find(miLetra) != -1) or (miLetra in letras_erradas):
             print('La letra ingresada ya ha sido descubierta')
             miLetra = input('Ingrese una letra: ')
 
@@ -47,13 +52,38 @@ def jugar(palabra):
             intentos -= 1
             if intentos > 0:
                 print('La letra no se encuentra en la palabra')
-                print(f'Te quedan {intentos} intentos')
+                letras_erradas.append(miLetra)
+                input('Presiona Enter para continuar')
             else:
                 print(f'Te quedaste sin intentos! La palabra era {palabra}')
                 break
 
 
-palabra = random.choice(palabras).lower()
-jugar(palabra)
-
-    
+Menu = True
+while Menu:
+    os.system('cls')
+    print('Bienvenido al juego del ahorcado!'.center(50,'-'))
+    print('''
+    1. Jugar
+    2. Salir
+    ''')
+    opcion = int(input('Ingrese una opcion: '))
+    if opcion == 1:
+        opcion2 = 'y'
+        while opcion2 == 'y':
+            palabra = random.choice(palabras).lower()
+            jugar(palabra)  
+            print('')
+            opcion2 = input('Queres jugar otra vez? (y/n): ')
+            while opcion2 != 'y' and opcion2 != 'n':
+                print('Entrada Invalida')
+                opcion2 = input('Queres jugar otra vez? (y/n): ')
+                os.system('cls')
+            if opcion2 == 'n':
+                print('Saliendo...')
+                Menu = False
+    elif opcion == 2:
+        print('Saliendo...')
+        Menu = False
+    else:
+        print('Entrada invalida')
